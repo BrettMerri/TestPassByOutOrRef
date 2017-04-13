a4c03c7c320ace0867a3eac9bf2b1d86a036cd11
BrettMerri <the.b.merri@gmail.com>
2017-04-13 12:57:25 -0400

    initial commit

---
+++ TestPassByOutOrRef/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPassByOutOrRef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Input = 3;
+            int S, C;
+
+            S = ReturnCubeSquare(Input, out C);
+
+            Console.WriteLine(S + "\n" + C);
+        }
+
+        public static int ReturnCubeSquare(int number, out int ReturnedCube)
+        {
+            int Square = number * number;
+            int Cube = number * number * number;
+
+            ReturnedCube = Cube; //return value of the cube back to the main
+            return Square;
+        }
+    }
+}
